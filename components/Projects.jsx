38a3f2b7e6cad024f73743f863a3103605eb3535
Akash Chaudhary <akashfzd2000@gmail.com>
2021-12-25 21:19:43 +0530

    projects section done

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../styles/Projects.module.css';
+import {FaEye,FaCode} from 'react-icons/fa'
 
 const Projects = () => {
   return (
@@ -12,7 +13,68 @@ const Projects = () => {
           <div className={styles.projectsCol}>
             <div className={styles.projectsCard}>
               <div className={styles.cardLeft}>
-                <img src='/images/cm.png' alt='' />
+                <img src='/images/cm1.png' alt='' />
+              </div>
+              <div className={styles.cardRight}>
+                <div className={styles.cardTitle}>
+                  <h1>Corona Meter</h1>
+                </div>
+                <div className={styles.TechRow}>
+                  <p>Flutter</p>
+                  <p>Firebase</p>
+                  <p>Firestore</p>
+                  <p>Android</p>
+                </div>
+                <div className={styles.cardBody}>
+                  An android application developed with Flutter SDK using Dart
+                  programming language that keeps you updated with real time
+                  COVID statistics.
+                </div>
+                <div className={styles.cardActions}>
+                  <div className={styles.viewButton}>
+                    <FaEye />
+                    <span>VIEW</span>
+                  </div>
+                  <div className={styles.codeButton}>
+                    <FaCode />
+                    <span>
+                      CODE
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.projectsCard}>
+              <div className={styles.cardLeft}>
+                <img src='/images/gke.png' alt='' />
+              </div>
+              <div className={styles.cardRight}>
+                <div className={styles.cardTitle}>
+                  <h1>GKeep(Task Manager)</h1>
+                </div>
+                <div className={styles.TechRow}>
+                  <p>React</p>
+                  <p>Redux</p>
+                  <p>Firebase</p>
+                  <p>Styled Components</p>
+                </div>
+                <div className={styles.cardBody}>
+                  An android application developed with Flutter SDK using Dart
+                  programming language that keeps you updated with real time
+                  COVID statistics.
+                </div>
+                <div className={styles.cardActions}>
+                  <div className={styles.viewButton}>
+                    <FaEye />
+                    <span>VIEW</span>
+                  </div>
+                  <div className={styles.codeButton}>
+                    <FaCode />
+                    <span>
+                      CODE
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
