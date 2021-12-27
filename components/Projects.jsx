bfbe47a58c2d001ae38d466cdf8699aec94770b5
Akash Chaudhary <akashfzd2000@gmail.com>
2021-12-27 20:19:08 +0530

    code refactoring

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from '../styles/Projects.module.css';
 import { FaEye, FaCode } from 'react-icons/fa';
+import { projectList } from '../public/data/projects';
 
 const Projects = () => {
+  const url = '/images/p.png';
+
   return (
     <>
       <div className={styles.projectsContainer} id='projects'>
@@ -11,122 +14,42 @@ const Projects = () => {
             <h1>MY WORK</h1>
           </div>
           <div className={styles.projectsCol}>
-            <div className={styles.projectsCard}>
-              <div className={styles.cardLeft}>
-                <img src='/images/cm1.png' alt='' />
-              </div>
-              <div className={styles.cardRight}>
-                <div className={styles.cardTitle}>
-                  <h1>Corona Meter</h1>
+            {projectList.map((project) => (
+              <div className={styles.projectsCard}>
+                <div className={styles.cardLeft}>
+                  <img src={project.imgURL} alt='' />
                 </div>
-                <div className={styles.TechRow}>
-                  <p>Flutter</p>
-                  <p>Firebase</p>
-                  <p>Firestore</p>
-                  <p>Android</p>
-                </div>
-                <div className={styles.cardBody}>
-                  An android application developed with Flutter SDK using Dart
-                  programming language that keeps you updated with real time
-                  COVID statistics.
-                </div>
-                <div className={styles.cardActions}>
-                  <a
-                    className={styles.viewButton}
-                    href='https://github.com/cyberlord-007/Corona-Meter-App'
-                    target='_blank'
-                  >
-                    <FaEye />
-                    <span>VIEW</span>
-                  </a>
-                  <a
-                    className={styles.codeButton}
-                    href='https://github.com/cyberlord-007/Corona-Meter-App'
-                    target='_blank'
-                  >
-                    <FaCode />
-                    <span>CODE</span>
-                  </a>
+                <div className={styles.cardRight}>
+                  <div className={styles.cardTitle}>
+                    <h1>{project.title}</h1>
+                  </div>
+                  <div className={styles.TechRow}>
+                    {project.tech.map((t) => (
+                      <p>{t}</p>
+                    ))}
+                  </div>
+                  <div className={styles.cardBody}>{project.desc}</div>
+                  <div className={styles.cardActions}>
+                    <a
+                      className={styles.viewButton}
+                      href={project.hostURL}
+                      target='_blank'
+                    >
+                      <FaEye />
+                      <span>VIEW</span>
+                    </a>
+                    <a
+                      className={styles.codeButton}
+                      href={project.repoURL}
+                      target='_blank'
+                    >
+                      <FaCode />
+                      <span>CODE</span>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={styles.projectsCard}>
-              <div className={styles.cardLeft}>
-                <img src='/images/gke.png' alt='' />
-              </div>
-              <div className={styles.cardRight}>
-                <div className={styles.cardTitle}>
-                  <h1>GKeep</h1>
-                </div>
-                <div className={styles.TechRow}>
-                  <p>React</p>
-                  <p>Redux</p>
-                  <p>Firebase</p>
-                  <p>Styled Components</p>
-                </div>
-                <div className={styles.cardBody}>
-                  A task manager application like Google Keep that lets you add
-                  tasks,pin important ones,archive tasks and delete them. The
-                  user interface is fully responsive and supports dark theme as
-                  well.
-                </div>
-                <div className={styles.cardActions}>
-                  <a
-                    className={styles.viewButton}
-                    href='https://gkeep-624d0.web.app/'
-                    target='_blank'
-                  >
-                    <FaEye />
-                    <span>VIEW</span>
-                  </a>
-                  <a
-                    className={styles.codeButton}
-                    href='https://github.com/cyberlord-007/gkeep'
-                    target='_blank'
-                  >
-                    <FaCode />
-                    <span>CODE</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={styles.projectsCard}>
-              <div className={styles.cardLeft}>
-                <img src='/images/p.png' alt='' />
-              </div>
-              <div className={styles.cardRight}>
-                <div className={styles.cardTitle}>
-                  <h1>Personal Portfolio</h1>
-                </div>
-                <div className={styles.TechRow}>
-                  <p>React</p>
-                  <p>Redux</p>
-                  <p>Firebase</p>
-                  <p>Styled Components</p>
-                </div>
-                <div className={styles.cardBody}>
-                  A single page application made with NextJs which is fully mobile responsive. It's a beautiful looking portfolio website with a few animations here and there. 
-                </div>
-                <div className={styles.cardActions}>
-                  <a
-                    className={styles.viewButton}
-                    href='https://gkeep-624d0.web.app/'
-                    target='_blank'
-                  >
-                    <FaEye />
-                    <span>VIEW</span>
-                  </a>
-                  <a
-                    className={styles.codeButton}
-                    href='https://github.com/cyberlord-007/gkeep'
-                    target='_blank'
-                  >
-                    <FaCode />
-                    <span>CODE</span>
-                  </a>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ public/data/projects.jsx
@@ -0,0 +1,29 @@
+export const projectList = [
+  {
+    id: 0,
+    title: 'Corona Meter',
+    tech: ['Flutter', 'Firebase', 'Firestore', 'Android'],
+    desc: 'An android application developed with Flutter SDK using Dart programming language that keeps you updated with real time COVID statistics.',
+    imgURL: '/images/cm1.png',
+    hostURL: 'https://github.com/cyberlord-007/Corona-Meter-App',
+    repoURL: 'https://github.com/cyberlord-007/Corona-Meter-App'
+  },
+  {
+    id: 1,
+    title: 'GKeep',
+    tech: ['React', 'Redux', 'Firebase', 'Styled Components'],
+    desc: 'A task manager application like Google Keep that lets you add tasks,pin important ones,archive tasks and delete them. The user interface is fully responsive and supports dark theme as well.',
+    imgURL: '/images/gke.png',
+    hostURL: 'https://gkeep-624d0.web.app/',
+    repoURL: 'https://github.com/cyberlord-007/gkeep'
+  },
+  {
+      id: 2,
+      title: 'Personal Portfolio',
+      tech: ['React','Next','HTML','CSS'],
+      desc: `A single page application made with NextJs which is fully mobile responsive. It's a beautiful looking portfolio website with a few animations here and there.`,
+      imgURL: '/images/p.png',
+      hostURL: 'https://cyberlord007.tech/',
+      repoURL: 'https://github.com/cyberlord-007/next-portfolio'
+  }
+];
