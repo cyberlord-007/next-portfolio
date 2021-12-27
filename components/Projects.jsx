6ec673cc01e6a75549cdc5ba8a4a3b94bea62506
Akash Chaudhary <akashfzd2000@gmail.com>
2021-12-27 20:01:04 +0530

    new project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -65,9 +65,47 @@ const Projects = () => {
                   <p>Styled Components</p>
                 </div>
                 <div className={styles.cardBody}>
-                  An android application developed with Flutter SDK using Dart
-                  programming language that keeps you updated with real time
-                  COVID statistics.
+                  A task manager application like Google Keep that lets you add
+                  tasks,pin important ones,archive tasks and delete them. The
+                  user interface is fully responsive and supports dark theme as
+                  well.
+                </div>
+                <div className={styles.cardActions}>
+                  <a
+                    className={styles.viewButton}
+                    href='https://gkeep-624d0.web.app/'
+                    target='_blank'
+                  >
+                    <FaEye />
+                    <span>VIEW</span>
+                  </a>
+                  <a
+                    className={styles.codeButton}
+                    href='https://github.com/cyberlord-007/gkeep'
+                    target='_blank'
+                  >
+                    <FaCode />
+                    <span>CODE</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className={styles.projectsCard}>
+              <div className={styles.cardLeft}>
+                <img src='/images/p.png' alt='' />
+              </div>
+              <div className={styles.cardRight}>
+                <div className={styles.cardTitle}>
+                  <h1>Personal Portfolio</h1>
+                </div>
+                <div className={styles.TechRow}>
+                  <p>React</p>
+                  <p>Redux</p>
+                  <p>Firebase</p>
+                  <p>Styled Components</p>
+                </div>
+                <div className={styles.cardBody}>
+                  A single page application made with NextJs which is fully mobile responsive. It's a beautiful looking portfolio website with a few animations here and there. 
                 </div>
                 <div className={styles.cardActions}>
                   <a
